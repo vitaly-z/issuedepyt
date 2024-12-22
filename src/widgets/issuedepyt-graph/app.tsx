0b68098237363ba535f439d71f6ecf35a38196a8
Marcus Christensson <marcus.christensson@smarteye.se>
2024-12-22 18:46:45 +0100

    First version

---
+++ src/widgets/issuedepyt-graph/app.tsx
@@ -0,0 +1,21 @@
+import React, {memo, useCallback} from 'react';
+import Button from '@jetbrains/ring-ui-built/components/button/button';
+
+// Register widget in YouTrack. To learn more, see https://www.jetbrains.com/help/youtrack/devportal-apps/apps-host-api.html
+const host = await YTApp.register();
+
+const AppComponent: React.FunctionComponent = () => {
+  const callBackend = useCallback(async () => {
+    const result = await host.fetchApp('backend/debug', {query: {test: '123'}});
+    // eslint-disable-next-line no-console
+    console.log('request result', result);
+  }, []);
+
+  return (
+    <div className="widget">
+      <Button primary onClick={callBackend}>{'Make HTTP Request'}</Button>
+    </div>
+  );
+};
+
+export const App = memo(AppComponent);
