83252d78c0635622d5461e1979441d0013a68c85
Marcus Christensson <marcus.christensson@gmail.com>
2025-03-08 19:14:13 +0100

    Fix filtering of nodes without links

--- src/widgets/issuedepyt-graph/issue-helpers.tsx
+++ src/widgets/issuedepyt-graph/issue-helpers.tsx
@@ -2,32 +2,38 @@ import { IssueInfo, IssueLink } from "./issue-types";
 import { FilterState } from "../../../@types/filter-state";
 
 export const filterIssues = (filter: FilterState, issues: Record<string, IssueInfo>) => {
-  const relations = Object.values(issues).flatMap((issue: IssueInfo) =>
-    [
-      ...(issue.showUpstream ? issue.upstreamLinks : []),
-      ...(issue.showDownstream ? issue.downstreamLinks : []),
-    ].map((link: IssueLink) => ({
-      from: issue.id,
-      to: link.targetId,
-    }))
+  const filteredIssues = Object.fromEntries(
+    Object.entries(issues).filter(([key, issue]) => {
+      const state = filter?.state || {};
+      const type = filter?.type || {};
+      const stateValue = issue?.state;
+      const typeValue = issue?.type;
+      return (
+        (stateValue == undefined || state[stateValue]) &&
+        (typeValue == undefined || type[typeValue])
+      );
+    })
   );
+
+  const relations = Object.entries(filteredIssues)
+    .map(([key, issue]) => issue)
+    .flatMap((issue: IssueInfo) =>
+      [
+        ...(issue.showUpstream ? issue.upstreamLinks : []),
+        ...(issue.showDownstream ? issue.downstreamLinks : []),
+      ].map((link: IssueLink) => ({
+        from: issue.id,
+        to: link.targetId,
+      }))
+    )
+    // Make sure that the target is visible.
+    .filter((x) => x.to in filteredIssues);
+
   return Object.fromEntries(
-    Object.entries(issues)
-      // Apply the filter.
-      .filter(([key, issue]) => {
-        const state = filter?.state || {};
-        const type = filter?.type || {};
-        const stateValue = issue?.state;
-        const typeValue = issue?.type;
-        return (
-          (stateValue == undefined || state[stateValue]) &&
-          (typeValue == undefined || type[typeValue])
-        );
-      })
-      // Only show issues that's shown in the dependency graph, i.e. that they have a visible relation.
-      .filter(
-        ([key, issue]) =>
-          issue.depth === 0 || relations.some((x) => x.from === issue.id || x.to === issue.id)
-      )
+    // Only show issues that's shown in the dependency graph, i.e. that they have a visible relation.
+    Object.entries(filteredIssues).filter(
+      ([key, issue]) =>
+        issue.depth === 0 || relations.some((x) => x.from === issue.id || x.to === issue.id)
+    )
   );
 };
