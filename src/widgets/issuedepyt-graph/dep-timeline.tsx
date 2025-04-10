6d2e5aa98819eb60860dd84b53ebec1c68919dde
Marcus Christensson <marcus.christensson@gmail.com>
2025-04-10 21:45:35 +0200

    timeline: Add grouping of items

--- src/widgets/issuedepyt-graph/dep-timeline.tsx
+++ src/widgets/issuedepyt-graph/dep-timeline.tsx
@@ -103,15 +103,17 @@ const DepTimeline: React.FunctionComponent<DepTimelineProps> = ({
   const containerRef = useRef<HTMLDivElement>(null);
   const timeline = useRef<VisTimeline>(null);
   const items = useRef<DataSet<TimelineItem>>(new DataSet());
+  const groups = useRef<DataSet<TimelineGroup>>(new DataSet());
 
   useEffect(() => {
-    if (containerRef.current && items.current) {
+    if (containerRef.current && items.current && groups.current) {
       const options = {
         width: "100%",
         autoResize: true,
+        showWeekScale: true,
       };
       // @ts-ignore
-      timeline.current = new Timeline(containerRef.current, items.current, options);
+      timeline.current = new Timeline(containerRef.current, items.current, groups.current, options);
       timeline.current.on("select", (props) => {
         const selectedItems = props.items;
         if (selectedItems != undefined && selectedItems.length > 0) {
@@ -132,7 +134,93 @@ const DepTimeline: React.FunctionComponent<DepTimelineProps> = ({
       const visibleIssues = Object.values(filterIssues(filterState, issues))
         // Only show issues with a start or end date.
         .filter((issue) => Object.values(getIssueStartEnd(issue)).filter((x) => !!x).length > 0);
-      console.log(`Rendering timeline with ${Object.keys(visibleIssues).length} nodes`);
+      const visibleIssueIds = new Set(visibleIssues.map((issue) => issue.id));
+
+      const getGroupIssueId = (issue: IssueInfo): string | undefined => {
+        const isSubtask = (link: IssueLink) => link.type === "Subtask";
+        const parentLinks = issue.downstreamLinks.filter(isSubtask);
+        const otherVisibleLinks = issue.downstreamLinks
+          .filter((link) => !isSubtask(link))
+          .filter((link) => visibleIssueIds.has(link.targetId));
+        const visibleParentLinks = parentLinks.filter((link) => visibleIssueIds.has(link.targetId));
+        if (visibleParentLinks.length > 0) {
+          return visibleParentLinks[0].targetId;
+        } else if (otherVisibleLinks.length > 0) {
+          return otherVisibleLinks[0].targetId;
+        } else if (parentLinks.length > 0) {
+          return parentLinks[0].targetId;
+        }
+        return undefined;
+      };
+
+      const groupIds = [
+        ...new Set(
+          Object.values(issues)
+            .map(getGroupIssueId)
+            .filter((x) => x != undefined)
+        ),
+      ];
+      console.log(`Group issue IDs: ${groupIds.map((x) => issues[x].idReadable).join(", ")}`);
+
+      const rootGroup: TimelineGroup = {
+        id: "root-group",
+        content: "Root",
+        visible: true,
+        // @ts-ignore
+        treeLevel: 0,
+        order: 0,
+        nestedGroups: [],
+      };
+      const groupItems: TimelineGroup[] = [rootGroup];
+      groupItems.push(
+        ...groupIds.map((id) => {
+          const issue = issues[id];
+          const upstreamGroupIssueIds = issue.upstreamLinks.filter((link) =>
+            groupIds.includes(link.targetId)
+          );
+          return {
+            id: `${id}-group`,
+            content: issue.idReadable,
+            title: `${issue.idReadable}: ${issue.summary}`,
+            visible: true,
+            treeLevel: issue.depth + 1,
+            order: issue.depth + 1,
+            nestedGroups:
+              upstreamGroupIssueIds.length > 0
+                ? upstreamGroupIssueIds.map((link) => `${link.targetId}-group`)
+                : undefined,
+          };
+        })
+      );
+      groupItems.push({
+        id: "no-group",
+        content: "No group",
+        visible: true,
+        // @ts-ignore
+        treeLevel: 0,
+        order: groupIds.length + 1,
+        nestedGroups: [],
+      });
+
+      // Find group with lowest depth which is not nested into another group and
+      // nest this into the root group.
+      const issuesThatAreGroupsWithoutParentGroup = groupIds
+        .filter(
+          (id) =>
+            groupItems.filter((x) => x.nestedGroups && x.nestedGroups.includes(`${id}-group`))
+              .length === 0
+        )
+        .map((id) => issues[id])
+        // Sort in ascending depth order.
+        .sort((a, b) => a.depth - b.depth);
+      if (issuesThatAreGroupsWithoutParentGroup.length > 0) {
+        const onlyLowestDepth = issuesThatAreGroupsWithoutParentGroup.filter(
+          (issue) => issue.depth === issuesThatAreGroupsWithoutParentGroup[0].depth
+        );
+        rootGroup.nestedGroups = onlyLowestDepth.map((issue) => `${issue.id}-group`);
+      }
+
+      console.log(`Rendering timeline with ${visibleIssues.length} nodes`);
       const stateColors = fieldInfo?.stateField ? fieldInfo.stateField.values : {};
       const stateStyles = Object.fromEntries(
         Object.entries(stateColors).map(([k, v]) => [
@@ -168,16 +256,45 @@ const DepTimeline: React.FunctionComponent<DepTimelineProps> = ({
         }
         className = isOverdue ? `${className}-overdue` : className;
         const estimate = issue?.estimation ? ` [${issue.estimation.presentation}]` : "";
+
+        let groupId = "root-group";
+        if (issue.depth > 0) {
+          const groupIssueId = getGroupIssueId(issue);
+          groupId = groupIssueId ? `${groupIssueId}-group` : "no-group";
+        }
+
         const item: TimelineItem = {
           id: issue.id,
           content: `${typeSymbol} ${issue.idReadable}: ${issue.summary}${estimate}${warningSign}`,
           title: getTooltip(issue, isOverdue),
           className,
           style: issue?.state && issue.state in stateStyles ? stateStyles[issue.state] : undefined,
+          group: groupId,
           ...itemPeriodProps,
         };
         return item;
       });
+
+      // Only include groups with items or nested groups.
+      const nonEmptyGroupIds = new Set(timelineItems.map((x) => x.group));
+      const finalGroupIds = new Set(
+        groupItems
+          .filter(
+            (x) => nonEmptyGroupIds.has(x.id) || (x.nestedGroups && x.nestedGroups.length > 0)
+          )
+          .map((x) => x.id)
+      );
+      const removedGroupIds = new Set(
+        groupItems.filter((group) => !finalGroupIds.has(group.id)).map((x) => x.id)
+      );
+      // Determine final group items and adjuste nestedGroups to only include valid groups.
+      const finalGroupItems: TimelineGroup[] = groupItems
+        .filter((x) => finalGroupIds.has(x.id))
+        .map((x: TimelineGroup) => ({
+          ...x,
+          nestedGroups: x.nestedGroups?.filter((y) => !removedGroupIds.has(y)) || undefined,
+        }));
+
       const currentIds = items.current.getIds();
       const idsToRemove = currentIds.filter((id) => !timelineItems.some((x) => x.id === id));
       const itemsToAdd = timelineItems.filter((x) => !currentIds.includes(x.id));
@@ -193,8 +310,11 @@ const DepTimeline: React.FunctionComponent<DepTimelineProps> = ({
       items.current.remove(idsToRemove);
       items.current.add(itemsToAdd);
       items.current.updateOnly(itemsToUpdate);
+
+      groups.current.clear();
+      groups.current.add(finalGroupItems);
       // @ts-ignore
-      timeline.current.setItems(items.current);
+      timeline.current.setData({ groups: groups.current, items: items.current });
       timeline.current.fit({ animation: false });
     }
   }, [issues, filterState]);
